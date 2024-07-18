2b9e97ae4295d91f3c59e96f960ecd09636dfb45
atman <gpbjk0304@gmail.com>
2024-07-18 22:04:05 +0900

    feat(ddd): blazor表示を追加

--- ddd/ddd-sample-app/DDDSampleApp.Wpf/MainWindow.xaml.cs
+++ ddd/ddd-sample-app/DDDSampleApp.Wpf/MainWindow.xaml.cs
@@ -1,23 +1,20 @@
-﻿using System.Text;
+﻿using Microsoft.Extensions.DependencyInjection;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
-namespace DDDSampleApp.Wpf;
-
-/// <summary>
-/// Interaction logic for MainWindow.xaml
-/// </summary>
-public partial class MainWindow : Window
+namespace DDDSampleApp.Wpf
 {
-    public MainWindow()
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
     {
-        InitializeComponent();
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            var serviceCollection = new ServiceCollection();
+            serviceCollection.AddWpfBlazorWebView();
+            Resources.Add("services", serviceCollection.BuildServiceProvider());
+        }
     }
 }
